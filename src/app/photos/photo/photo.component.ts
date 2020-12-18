973da3a3e80be40eb1a7a64e471a7a1e58ce6e9d
Lucas Ventura <lucas.alves@mobiauto.com.br>
2020-12-18 17:13:10 -0300

    Add Photos Upload; Fix Router Guards

--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -1,12 +1,22 @@
 import { Component, Input } from '@angular/core';
 
+const CLOUD = 'http://localhost:3000/imgs/';
+
 @Component({
     selector: 'ap-photo',
     templateUrl: 'photo.component.html'
 })
 export class PhotoComponent {
     
+    private _url = '';
+
     @Input() description='';
     
-    @Input() url='';
+    @Input() set url(url: string) {
+        this._url = (!url.startsWith('data') ? CLOUD : '') + url;
+    }
+
+    get url() {
+        return this._url;
+    }
 }
